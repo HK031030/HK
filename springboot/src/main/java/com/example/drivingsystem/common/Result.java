5f0069e38b5b91fb56c4dd3ce79909d69949818c
JQH <hkqh031030@gmail.com>
2025-02-21 14:34:07 +0800

    增删改查

--- springboot/src/main/java/com/example/drivingsystem/common/Result.java
+++ springboot/src/main/java/com/example/drivingsystem/common/Result.java
@@ -1,74 +1,43 @@
 package com.example.drivingsystem.common;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 /**
- * 定义统一返回类型
+ * 接口统一返回包装类
  */
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class Result {
 
-    private String code;
-    private String msg;
-    private Object data;
 
-    public Result() {
+    public static final String CODE_SUCCESS = "200";
+    public static final String CODE_AUTH_ERROR = "401";
+    public static final String CODE_SYS_ERROR = "500";
 
-    }
-
-    public String getCode() {
-        return code;
-    }
-
-    public void setCode(String code) {
-        this.code = code;
-    }
-
-    public String getMsg() {
-        return msg;
-    }
-
-    public void setMsg(String msg) {
-        this.msg = msg;
-    }
-
-    public Object getData() {
-        return data;
-    }
-
-    public void setData(Object data) {
-        this.data = data;
-    }
+    private String code;  //请求的返回码
+    private String msg;   //错误的详细信息
+    private String data;  //数据从什么地方返回出去
 
     public static Result success() {
-        Result result = new Result();
-        result.setCode("200");
-        result.setMsg("请求成功");
-        return result;
+        return new Result(CODE_SUCCESS, "请求成功", null);
     }
 
     public static Result success(Object data) {
-        Result result = success();
-        result.setData(data);
-        return result;
-    }
-
-    public static Result error() {
-        Result result = new Result();
-        result.setCode("500");
-        result.setMsg("请求失败");
-        return result;
-    }
-
-    public static Result error(String code, String msg) {
-        Result result = new Result();
-        result.setCode(code);
-        result.setMsg(msg);
-        return result;
+        return new Result(CODE_SUCCESS, "请求成功", data);
     }
 
     public static Result error(String msg) {
-        Result result = new Result();
-        result.setCode("500");
-        result.setMsg(msg);
-        return result;
+        return new Result(CODE_SYS_ERROR, msg, null);
     }
-}
 
+
+
+
+}
